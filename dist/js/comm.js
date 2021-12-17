0495437c7ff95ced0df37cd22f1d11f9aceb4a39
eehd80 <eehd80yul@gmail.com>
2021-12-17 15:22:47 +0900

    dd

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,1 +1,27 @@
-$(function () {});
+$(function () {
+    // $(".select-module").each(function () {
+    //     let $this = $(this),
+    //         $boxModule = $this.find("li"),
+    //         $boxLink = $this.find("a");
+
+    //     $boxModule.removeClass("on");
+
+    //     $boxModule.click(function () {
+    //         // event.preventDefault();
+
+    //         let $this = $(this);
+
+    //         if ($this.hasClass("on") === true) {
+    //             $this.removeClass("on");
+    //         } else {
+    //             $this.addClass("on");
+    //         }
+
+    //         // if ($boxModule.hasClass("on") === false) {
+    //         //     $boxModule.addClass("on");
+    //         // } else {
+    //         //     $this.removeClass("on");
+    //         // }
+    //     });
+    // });
+});
